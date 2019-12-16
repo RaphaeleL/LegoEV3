6061dd8839af332a4e11d1fd5097b4b0718478a7
Raphaele Salvatore Licciardo <48721847+RaphaeleL@users.noreply.github.com>
2019-12-16 12:24:06 +0100

    Clean up Leader Code

--- Leader/Ch1_Leader/main.py
+++ Leader/Ch1_Leader/main.py
@@ -11,51 +11,28 @@ class HotRod():
         self.engine, self.wheels, self.infrared = Motor(Port.D), Motor(Port.A), InfraredSensor(Port.S4)
         self.speed_of_engine, self.speed_of_wheels, self.time_of_wheels = speed_e, speed_w, time_w
         self.brick = brick
-
-    def setBeep(self, pitch, time, volume): 
-        self.brick.sound.beep(pitch, time, volume)
-
-    def setSound(self, soundfile): 
-        self.brick.sound.file(soundfile)
-
     def setDisplay(self, imagefile, text, pos):
         self.brick.display.clear()
         self.brick.display.image(imagefile)
         self.brick.display.text(text, pos)
-
-    def setLight(self, color): 
-        self.brick.light(color)
-
-    def stopEngine(self): 
-        self.engine.stop() 
-
-    def stopWheels(self): 
-        self.wheels.stop()
-
     def stopEverything(self):
         self.stopEngine()
         self.stopWheels()
-
-    def forward(self): 
-        self.engine.run(self.speed_of_engine)
-
-    def moveLeft(self): 
-        self.wheels.run_time(self.speed_of_wheels, self.time_of_wheels)
-
-    def moveRight(self): 
-        self.wheels.run_time(-1 * self.speed_of_wheels, self.time_of_wheels)
-
-    def checkBattery(self, limit): 
-        if self.brick.battery.voltage() < limit: self.brick.sound.beep()
-
+    def forward(self): self.engine.run(self.speed_of_engine)
+    def moveLeft(self): self.wheels.run_time(self.speed_of_wheels, self.time_of_wheels)
+    def moveRight(self): self.wheels.run_time(-1 * self.speed_of_wheels, self.time_of_wheels)
+    def checkBattery(self, limit): if self.brick.battery.voltage() < limit: self.brick.sound.beep()
+    def setLight(self, color): self.brick.light(color)
+    def stopEngine(self): self.engine.stop()
+    def stopWheels(self): self.wheels.stop()
+    def setBeep(self, pitch, time, volume): self.brick.sound.beep(pitch, time, volume)
+    def setSound(self, soundfile): self.brick.sound.file(soundfile)
     def start(self):
         self.setBeep(1500, 1000, 50)
         self.setSound(SoundFile.HELLO)
         self.setDisplay(ImageFile.UP, "HotRod", (60,10))
-
         while True: 
             distance_to_front = self.infrared.distance()
-
             if distance_to_front < 50:
                 self.stopEverything()
                 self.setSound(SoundFile.ERROR_ALARM)
@@ -64,9 +41,7 @@ class HotRod():
             else:
                 self.setLight(Color.GREEN)
                 self.setDisplay(ImageFile.UP,"HotRod", (60,10))
-
                 pressed_key = self.infrared.buttons(1)
-                
                 for button in pressed_key: 
                     if button == 128: self.forward()
                     elif button == 2: self.stopEngine()
@@ -74,7 +49,6 @@ class HotRod():
                     elif button == 8: self.moveRight()
                     else: self.stopEverything() 
             self.checkBattery(7000)
-
 hr = HotRod(Motor(Port.D), Motor(Port.A), InfraredSensor(Port.S4), 2000, 500, 250)
 hr.start()
 
