ecef9d26fec459b9b7bceb590d5bb5106da8ba87
Raphaele_Salvatore Licciardo <lira1011@hs-karlsruhe.de>
2019-12-20 08:14:19 +0100

    Remove: All Way Stearing from leader

--- Leader/Leader/main.py
+++ Leader/Leader/main.py
@@ -7,9 +7,9 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
 engine, wheels, infrared = Motor(Port.D), Motor(Port.A), InfraredSensor(Port.S4)
-i_left = InfraredSensor(Port.S2)
-i_right = InfraredSensor(Port.S3)
-i_back = InfraredSensor(Port.S1)
+#i_left = InfraredSensor(Port.S2)
+#i_right = InfraredSensor(Port.S3)
+#i_back = InfraredSensor(Port.S1)
 speed_of_engine, speed_of_wheels, time_of_wheels = 2000, 500, 250
 
 brick.sound.beep(1500, 1000, 50)
@@ -48,10 +48,24 @@ while True:
         brick.display.clear()
         brick.display.image(ImageFile.UP)
         brick.display.text("HotRod", (60, 10))
+        '''
         move(infrared)
         move(i_left)
         move(i_right)
         move(i_back)
-        
+        '''
+        pressed_key = infrared.buttons(1)
+        for button in pressed_key: 
+            if button == 128: 
+                engine.run(speed_of_engine)
+            elif button == 2: 
+                engine.stop()
+            elif button == 512: #left
+                wheels.run_time(speed_of_wheels, time_of_wheels)
+            elif button == 8: #right
+                wheels.run_time(-1 * speed_of_wheels, time_of_wheels)
+            else: 
+                engine.stop()
+                wheels.stop()
     if brick.battery.voltage() < 7000: 
         brick.sound.beep()
