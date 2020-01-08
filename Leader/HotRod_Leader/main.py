d5eadeac30181beb745a66ca25bee13ec1145666
Raphaele Salvatore Licciardo <48721847+RaphaeleL@users.noreply.github.com>
2020-01-08 15:21:19 +0100

    Clean code and finish color sensor

--- Leader/HotRod_Leader/main.py
+++ Leader/HotRod_Leader/main.py
@@ -9,73 +9,71 @@ from pybricks.robotics import DriveBase
 
 # Init of Engine
 engine, wheels, infrared = Motor(Port.D), Motor(Port.A), InfraredSensor(Port.S4)
-#color_1, color_2 = ColorSensor(Port.S1), ColorSensor(Port.S2)
+color_1, color_2 = ColorSensor(Port.S1), ColorSensor(Port.S2)
+
 # Init of Variables
 speed_of_engine, speed_of_wheels, time_of_wheels = 2000, 500, 300
 
-# Start Procedure
-# - Make a Beep :-)
-brick.sound.beep(1500, 1000, 50)
-# - Say Hello
-brick.sound.file(SoundFile.HELLO)
-# - Write open eyes and "HotRod" on the Display
-brick.display.clear()
-brick.display.image(ImageFile.UP)
-brick.display.text("HotRod", (60, 10))
+def attentionPlease(soundfile, imagefile, color, text, pos, beep):
+    # Make a Beep :-)
+    if beep: brick.sound.beep(1500, 1000, 50)
+
+    # Say Hello
+    brick.sound.file(soundfile)
+
+    # Make Red Light on the Brick
+    brick.light(color)
+
+    # Write open eyes and "HotRod" on the Display
+    brick.display.clear()
+    brick.display.image(imagefile)
+    brick.display.text(text, pos)
+
+# Start Procedure --> Hey User, all okay. I'm Ready now.
+attentionPlease(SoundFile.HELLO, ImageFile.UP, Color.GREEN, "Hotrod", (60,10), True)
 
 # Infinity Loop
-while True: 
+while True:
+
     # Messure the Distance to the front
     distance_to_front = infrared.distance()
-    #color_of_c1 = color_1.color()
-    #color_of_c2 = color_2.rgb()
-    # Is the Distance Okay? NO!
-    if distance_to_front < 50: 
+    color_of_c1 = color_1.color()
+    color_of_c2 = color_2.rgb()
+
+    # Check if there are any Obstacles or something read
+    if distance_to_front < 50 or color_of_c1 == Color.RED or color_of_c2 == Color.RED:
         # Stop Engine
         engine.stop()
         wheels.stop()
-        # - Make Alarm --> Possible Crash
-        brick.sound.file(SoundFile.ERROR_ALARM)
-        # - Make Red Light on the Brick
-        brick.light(Color.RED)
-        # - Write closed eyes and "HotRod" on the Display
-        brick.display.clear()
-        brick.display.image(ImageFile.KNOCKED_OUT)
-        brick.display.text("HotRod", (60, 10))
+
+        # Troubleshooting for the User
+        attentionPlease(SoundFile.ERROR_ALARM, ImageFile.KNOCKED_OUT, Color.RED, "Hotrod", (60,10), False)
+
     # Distance is Okay. You can drive
-    else: 
-        # - Make Green Light on the Bright
-        brick.light(Color.GREEN)
-        # - Write open eyes and "HotRod" on the Display
-        brick.display.clear()
-        brick.display.image(ImageFile.UP)
-        brick.display.text("HotRod", (60, 10))
+    else:
+
+        # Feedback for the User --> All okay
+        attentionPlease(SoundFile.SNEEZING, ImageFile.GREEN, Color.UP, "Hotrod", (60,10), False)
+
         # Get the pushed Buttons of the Remote Controll
         pressed_key = infrared.buttons(1)
+
         # Iterate these list
-        for button in pressed_key: 
+        for button in pressed_key:
+
             # Straight --> Upper Left
-            if button == 128: 
-                engine.run(speed_of_engine)
+            if button == 128: engine.run(speed_of_engine)
             # Break --> Bottom Left
-            elif button == 2: 
-                engine.stop()
+            elif button == 2: engine.stop()
             # Left --> Bottom Right
-            elif button == 512:
-                wheels.run_time(speed_of_wheels, time_of_wheels)
+            elif button == 512: wheels.run_time(speed_of_wheels, time_of_wheels)
             # Right --> Upper Right
-            elif button == 8:
-                wheels.run_time(-1 * speed_of_wheels, time_of_wheels)
+            elif button == 8: wheels.run_time(-1 * speed_of_wheels, time_of_wheels)
             # Nothing pressed
-            else: 
-                engine.stop()
-                wheels.stop()
-        #if color_of_c1 == Color.RED:
-            #engine.stop()
-            #brick.sound.file(SoundFile.RED)
-        #if color_of_c2 == Color.BLUE:
-            #engine.stop()
-            #brick.sound.file(SoundFile.BLUE)
+            else: wheels.stop()
+                #engine.stop()
+                #wheels.stop()
+
     # Are the Battery Okay?
     if brick.battery.voltage() < 7000:
         # Make a Beep :(
